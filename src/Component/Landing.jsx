8e398046408bba5fcc22fc8c4ba466887600e94c
unknown <agehad248@gmail.com>
2025-01-25 20:52:25 +0200

    first commit

---
+++ src/Component/Landing.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+import { TypeAnimation } from 'react-type-animation';
+
+// import Button
+import Button from 'react-bootstrap/Button';
+import { motion } from 'framer-motion';
+
+//import imge
+import me from '../imges/me.jpg'
+
+const Landing = () => {
+    return (
+        <div className='Landing '>
+            <Container>
+                <Row className='d-flex  align-items-center pt-5'>
+                    <Col lg={6} className='mt-5 pt-5'>
+                        <h1 >Hi, I'm Ahmed Gehad
+                        </h1>
+                        <h2 className='py-3'>
+                            <TypeAnimation
+                                sequence={[
+                                    // Same substring at the start will only be typed out once, initially
+                                  
+                                    'Front-end Developer',
+                                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                
+                                ]}
+                                wrapper="span"
+                                speed={30}
+                                style={{ fontSize: '1em', display: 'inline-block' }}
+                                repeat={Infinity}
+                            />
+                        </h2>
+                        <p className='lead'>with 2+ years of experience
+                            <br />
+                            using React and Node.js.
+                            <br />
+                            Reach out if you'd like to learn more!
+                        </p>
+
+                        <motion.button
+                            whileTap={{ scale: 0.65 }} className='motionBtn '>
+                            <Button to="ContactPage" >Contact Me</Button>
+                        </motion.button>
+                    </Col>
+                    <Col lg={6} className=' py-5'>
+                        <img src={me} alt="" className='img ms-5 rounded-5' />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Landing
---
+++ src/Component/Navv.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import logo from '../imges/logo.jpg'
+import { Link } from 'react-router';
+
+// import Button
+import Button from 'react-bootstrap/Button';
+import { motion } from 'framer-motion';
+
+function Navv() {
+    return (
+
+        <div className='navBar '>
+            <Navbar expand="lg" className="">
+                <Container>
+                    <Navbar.Brand href="#home"> <img
+                        src={logo}
+                        width="100"
+                        height="80"
+                        className="d-inline-block align-top rounded-circle "
+                        alt="Ahmed gehad"
+                    /></Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+                        <Nav className="align-items-center">
+                            <Nav.Link href="#Home" className='link me-3 text-white'>Home</Nav.Link>
+                            <Nav.Link href="#About" className='link me-3 text-white'>About</Nav.Link>
+                            <Nav.Link href="#Experience" className='link me-3 text-white'>Experience</Nav.Link>
+                            <Nav.Link href="#Projects" className='link me-3 text-white'>Projects</Nav.Link>
+                            <Nav.Link href="#Contact" className='link me-3 text-white'>Contact</Nav.Link>
+                        </Nav>
+                       
+                            <motion.button
+                                whileTap={{ scale: 0.65 }} className='motionBtn '>
+                                <Button variant="success" to="ContactPage" className='ms-5'>Downlod CV</Button>
+                            </motion.button>
+                        
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </div>
+    )
+}
+
+export default Navv
